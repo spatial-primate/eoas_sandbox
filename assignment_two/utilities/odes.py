ad85f8d3cf0d923092b9e14b1268242b5a3fb5f6
Cara B G James <carajames@outlook.com>
2022-11-06 17:24:38 -0800

    couplings code (needs checking)

--- assignment_two/utilities/odes.py
+++ assignment_two/utilities/odes.py
@@ -1,29 +1,45 @@
 import numpy as np
-from utilities.volcanoes import volcanoes_scenario
+from utilities.emissions import emissions
+from data.constants import *
 
 
-def dtemp_dt(time, temp_array, k, volcano_model=None):
+def dtemperature_dtime(time: float, temperatures: np.ndarray,
+                       gamma: np.ndarray, sigma, tau, epsilon,
+                       albedo_sky: np.ndarray, albedo_surface: np.ndarray,
+                       solar=None, volcano_model=None, kwargs=None):
     """
     t: float (scalar), time.
-    T: size 6 array, temperature of boxes 1, 2, 3, 4, 5, 6.
-
-    dtempdt: size 6 array, change in temperature for boxes 1, 2, 3, 4, 5, 6.
+    temperature_array: zonally-averaged surface temperature (1 by 6)
+    dtemperature_dtime: (1 by 6) vector of changes to temperature
     """
+    # compute pre-factor on temperature
+    # sigma is constant
+    #changed name to b as k is taken for transfer coefficients
+    b = sigma * tau * epsilon 
+
+    # compute coupling terms (Luke - feel free to do this with your nice matrices if you prefer)
+    coup_pre = np.divide(1,(area*densities*thermals*specific_heats*)) # not sure if this will find the constants?...
+    coup_pos = np.ndarray((1,6))
+    coup_neg = np.ndarray((1,6))
+    for i in range(0,6): 
+            coup_pos[i] = k[i]*L[i]*(temperatures[i+1] - temperatures[i])
+            coup_neg[i+1] = k[i]*L[i]*(temperatures[i+1] - temperatures[i])
+            
+    couplings = coup_pre * (coup_pos - coup_neg)
 
     # Update values for fluxes from mass:
-    flux_array = np.multiply(k, temp_array)
+    flux_in = np.multiply(gamma, (1 - albedo_sky), (1 - albedo_surface), solar)
+    flux_out = np.multiply(b, temperatures ** 4)
 
-    flux_in = np.sum(flux_array, axis=1)  # row-wise for in-flux
-    flux_out = np.sum(flux_array, axis=0)  # column-wise for out-flux
+    dtemperature = flux_in - flux_out + couplings 
 
-    dtempdt = flux_in - flux_out
-
-    if volcanoes_model:
-        volcano_array = volcanoes_scenario([time], model=volcanoes_model)[0]
+    # todo: volcano-climate addition
+    if volcano_model:
+        emission_array = emissions([time], model=volcano_model)[0]
     else:
-        volcano_array = 0
+        emission_array = 0
 
     # add forcing fluxes to first slot: the atmosphere box
-    dtempdt[0] += volcano_array
+    dtemperature[0] += emission_array
 
-    return dtempdt
+    return dtemperature
