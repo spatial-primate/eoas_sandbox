c13ecf33daaa8dcd92cea8dfe6c9a0d344c04d3e
spatial-primate <lbrown@quantumspatial.com>
2022-11-09 17:43:40 -0800

    - improved logical ordering of computation in odes.py

--- assignment_two/solvers/odes.py
+++ assignment_two/solvers/odes.py
@@ -15,23 +15,32 @@ def dtemperature_dtime(time: float, temperatures: np.ndarray,
     returns:
     dtemperature_dtime: (1 by 6) vector of changes to temperature
     """
-
     # compute pre-factor on temperature term
     # sigma and tau are constant, epsilon usually just 1 (one)
     b = sigma * tau * epsilon
 
     noncoupling_prefactors = np.divide(1,
-                                       densities * thermals * specific_heats)
+                                       densities * thermals * specific_heats
+                                       )
 
-    # include heat transfer between zones
-    if compute_couplings:
+    # Update values for fluxes from new temperatures:
+    # todo: time-dependent albedo_sky term will plug-in here:
+    step1 = np.multiply(gamma.reshape(6), (1 - albedo_sky))
+    step2 = np.multiply(step1, (1 - albedo_surface))
+    flux_in = np.multiply(step2, S_0)
+    flux_out = np.multiply(b, temperatures ** 4)
+
+    if compute_couplings:  # include heat transfer between zones
         coupling_prefactors = np.divide(1,
-                                        area.reshape(6) * densities * thermals * specific_heats)
-        couplings_plus = np.zeros((6,))
-        couplings_minus = np.zeros((6,))
+                                        area.reshape(6) * densities * thermals * specific_heats
+                                        )
 
         # transfer couplings between zones saved as csv read in as constants
         couplings = np.dot(k_matrix, temperatures)
+
+        # relic
+        # couplings_plus = np.zeros((6,))
+        # couplings_minus = np.zeros((6,))
 
         # # do the north and south separately cuz they're built different
         # couplings_plus[0] = k[0] * L[0] * (temperatures[1] - temperatures[0])
@@ -41,19 +50,13 @@ def dtemperature_dtime(time: float, temperatures: np.ndarray,
         #     couplings_plus[zone] = k[zone] * L[zone] * (temperatures[zone + 1] - temperatures[zone])
         #     couplings_minus[zone] = k[zone - 1] * L[zone - 1] * (temperatures[zone] - temperatures[zone - 1])
         # couplings = couplings_plus - couplings_minus
+
     else:  # ignore heat transfer between zones
         couplings = 0.0
         coupling_prefactors = 0.0
 
     # scale couplings by zonally-averaged prefactors
     couplings = np.multiply(coupling_prefactors, couplings)
-
-    # Update values for fluxes from new temperatures:
-    # todo: time-dependent albedo_sky term will plug-in here:
-    step1 = np.multiply(gamma.reshape(6), (1 - albedo_sky))
-    step2 = np.multiply(step1, (1 - albedo_surface))
-    flux_in = np.multiply(step2, S_0)
-    flux_out = np.multiply(b, temperatures ** 4)
 
     dtemperature = noncoupling_prefactors * (flux_in - flux_out) + couplings
 
